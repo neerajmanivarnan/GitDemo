664d3262d113055600cbbe16ec0000a845e87177
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2023-05-19 00:00:55 +0530

    new files added

---
+++ JavaRecur/countZeros.java
@@ -0,0 +1,21 @@
+public class countZeros {
+    public static void main(String[] args) {
+            System.out.println(count(1231231));
+    }
+
+    public static int count(int n){
+        return helper(n,0);
+    }
+
+    private static int helper(int n, int c) {
+        if(n == 0){
+            return c;
+        }else{
+            if(n%10 ==0){
+                return helper(n/10,c+1);
+            }else{
+                return helper(n/10,c);
+            }
+        }
+    }
+}
